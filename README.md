ee45b6dff7d32e8d93addd50a522a40eaa44f3fb
Pranay Dilip Salkar <salkarpranay2@gmail.com>
2024-02-15 20:04:42 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Digital_CV
+# Digital Resume
+
+Link for my digital resume: https://darshans-resume.streamlit.app/
+
+## Introduction
+
+Welcome to my digital resume! This project showcases my skills, experience, and projects in an interactive and visually appealing manner. It goes beyond the limitations of a static document and serves as an online portfolio which allows you to explore my professional background and accomplishments.
+
+## Features
+
+- Interactive navigation: Easily navigate through different sections of the resume.
+- Project showcase: Discover my notable projects with descriptions and links.
+- Profile picture: Get a glimpse of the person behind the resume.
+- Contact information: Find ways to get in touch with me for collaboration or inquiries.
+
+## Technologies Used
+
+- Python: Streamlit framework for building the web application.
+- HTML/CSS: Styling and layout of the web pages.
+- PIL (Python Imaging Library): Displaying images, including the resume and profile picture.
+
+
+Note:- Every section which may include certificates are clickable so please click on the text itself to view the certificates.
+
+Thank you for visiting my digital resume! I hope you find it informative and engaging. If you have any questions or would like to connect, feel free to reach out to me.
---
+++ app.py
@@ -0,0 +1,133 @@
+from pathlib import Path
+
+import streamlit as st
+from PIL import Image
+
+
+# --- PATH SETTINGS ---
+current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
+css_file = current_dir / "styles" / "main.css"
+resume_file = current_dir / "assets" / "Pranay Salkar(CV).pdf"
+profile_pic = current_dir / "assets" / "My_Pic.png"
+
+
+# --- GENERAL SETTINGS ---
+PAGE_TITLE = "Digital CV | Pranay Dilip Salkar"
+PAGE_ICON = "📋"
+NAME = "Pranay Dilip Salkar"
+DESCRIPTION = """
+Data Science Enthusiast, seeking to gain hands-on experience in data science and leverage my skills to drive value for organizations.
+"""
+
+PROJECTS = {
+"🏆 Car Price Predictor- Perfect prdiction on Car prices": "https://pranay281202-car-price-predictor-app-oduaky.streamlit.app/",
+"🏆 HR Analytics using Power BI - Detailed analysis on specific HR Problem": "https://github.com/Pranay281202/HR-Analysis-using-PowerBI/blob/main/HR%20Analytics.pbix",
+"🏆 Credit Score Classification - prediction of Credit Score using Machine Learning model": "https://github.com/Pranay281202/Credit-Score-Classification/blob/main/Credit%20score%20classification%20project.ipynb",
+}
+
+st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
+
+# --- LOAD CSS, PDF & PROFILE PIC ---
+with open(css_file) as f:
+    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+with open(resume_file, "rb") as pdf_file:
+    PDFbyte = pdf_file.read()
+profile_pic = Image.open(profile_pic)
+
+
+# --- HERO SECTION ---
+col1, col2 = st.columns(2, gap="small")
+with col1:
+    st.write("\n")
+    st.image(profile_pic, width=320)
+
+with col2:
+    st.title(NAME)
+    st.write(DESCRIPTION)
+    # For Resume pdf Download
+    st.download_button(
+        label=" 📄 Download Resume",
+        data=PDFbyte,
+        file_name=resume_file.name,
+        mime="application/octet-stream",
+    )
+
+# --- EXPERIENCE & QUALIFICATIONS ---
+st.write('\n')
+st.subheader("Qualifications")
+
+st.write("- ✔️ BSc Data Science | KES Shroff College")
+st.write("- - "+"2020 - 2023")
+st.write("- ✔️ HSC | Sathaye College (Science)")
+st.write("- - "+"2018 - 2020")
+st.write("- ✔️ SSC | Marol Education Academy")
+st.write("- - "+"2017 - 2018")
+
+# --- SKILLS ---
+st.write('\n')
+st.subheader("Skills")
+col1,col2 = st.columns(2)
+with col1:
+    st.write(
+        """
+    - ➡️ Python
+    - ➡️ SQL
+    - ➡️ Machine Learning
+    - ➡️ Power BI/Tableau
+    - ➡️ NLP
+    """
+    )
+with col2:
+    st.write(
+        """
+        - ➡️ Data Analysis
+        - ➡️ Data Visualization
+        - ➡️ Business acumen
+        - ➡️ Problem-solving
+        """
+    )
+
+# --- WORK HISTORY ---
+st.write('\n') 
+st.subheader("Work Experience")
+st.write("---")
+
+st.write("💻 [Machine Learning Intern by **Bharat Intern**](https://drive.google.com/file/d/19tqgShvHQhq19GtaU9I0u3sa0bzWySgn/view?usp=drive_link)")
+
+# --- Projects & Accomplishments ---
+st.write('\n')
+st.subheader("Projects & Accomplishments")
+st.write("---")
+for project, link in PROJECTS.items():
+    st.write(f"[{project}]({link})")
+
+# --- Certificates ---
+st.write('\n')
+st.subheader("Certifications")
+st.write("---")
+st.write("⭐ [SQL Basic by Great learning](https://www.linkedin.com/posts/pranay-salkar-596899214_1000-free-courses-with-free-certificates-activity-7080074138630897665-40F0?utm_source=share&utm_medium=member_desktop)")
+st.write("⭐ [Advanced MS – Excel By KES Shroff College](https://drive.google.com/file/d/1Qsd7ftgDbqns-CN0_Kkga7Dtxzxtdw4V/view?usp=sharing)")
+st.write("⭐ [Recommender System By KES Shroff College](https://drive.google.com/file/d/1Kv2hYn9hG6-w2aZBciz3RIZnJlzSDIaN/view?usp=drive_link)")
+st.write('')
+
+# Create columns for each social media link
+col1, col2, col3, col4 = st.columns(4)
+
+# Add LinkedIn link
+with col1:
+    st.markdown('<a href="www.linkedin.com/in/pranay-salkar-596899214" target="_blank" style="text-decoration:none;"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/600px-LinkedIn_logo_initials.png?20140125013055" alt="LinkedIn" width="40" height="40" style="display:block;margin-left:auto;margin-right:auto;"></a>', unsafe_allow_html=True)
+    st.markdown('<p style="text-align:center;font-size:16px;margin-top:10px;">LinkedIn</p>', unsafe_allow_html=True)
+
+# Add GitHub link
+with col2:
+    st.markdown('<a href="https://github.com/Pranay281202" target="_blank" style="text-decoration:none;"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub" width="40" height="40" style="display:block;margin-left:auto;margin-right:auto;"></a>', unsafe_allow_html=True)
+    st.markdown('<p style="text-align:center;font-size:16px;margin-top:10px;">GitHub</p>', unsafe_allow_html=True)
+
+# Add Email link
+with col4:
+    st.markdown('<a href="mailto:salkarpranay2@gmail.com"  target="_blank" style="text-decoration:none;"><img src="https://workspace.google.com/static/img/products/png/gmail.png?cache=f50ecb6" alt="Email" width="40" height="40" style="display:block;margin-left:auto;margin-right:auto;"></a>', unsafe_allow_html=True)
+    st.markdown('<p style="text-align:center;font-size:16px;margin-top:10px;">Email</p>', unsafe_allow_html=True)
+
+# Add footer
+st.write('---')
+st.write('© Pranay Dilip Salkar  |  Last updated: February 2024')
