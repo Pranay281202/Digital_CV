564d8abb39044d13370b1db30a6e5bff21f56fd2
Pranay Dilip Salkar <salkarpranay2@gmail.com>
2024-02-15 20:18:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from PIL import Image
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
 resume_file = current_dir / "assets" / "Pranay Salkar(CV).pdf"
-profile_pic = current_dir / "assets" / "My_Pic.png"
+profile_pic = current_dir / "assets" / "My_pic.png"
 
 
 # --- GENERAL SETTINGS ---
