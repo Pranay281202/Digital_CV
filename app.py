3dc71050f2808537475d64fd315a833325fae0c9
Pranay Dilip Salkar <salkarpranay2@gmail.com>
2024-03-30 08:59:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import streamlit as st
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
-resume_file = current_dir / "assets" / "PRANAY_CV.pdf"
+resume_file = current_dir / "assets" / "Pranay-Resume.pdf"
 profile_pic = current_dir / "assets" / "My_pic.png"
 
 
