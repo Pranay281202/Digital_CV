a00311e7aba5ce7deb83c8dabf44a049feb732ec
Pranay Dilip Salkar <salkarpranay2@gmail.com>
2024-04-06 16:34:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ PAGE_ICON = "📋"
 NAME = "Pranay Dilip Salkar"
 DESCRIPTION = """
 As a recent graduate in Data Science, my goal is to dive into the corporate world to put my knowledge into action and grow as a professional.
-I'm passionate about data science and eager to get practical experience. I want to use my skills to help organizations succeed.
+Passionate about data science and eager to get practical experience and want to use my skills to help organizations succeed.
 """
 
 PROJECTS = {
